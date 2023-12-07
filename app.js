0292ca83a98dc6c0b282689178a148f054711dfc
Losara2023 <arabest2023@gmail.com>
2023-12-08 00:35:41 +0100

    Add Mangoose & MangoDB

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/',(req,res) => { 
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+mongoose
+  .connect(
     
-    res.sendFile('./views/home.html',{root:__dirname})
- })
-
-
-app.listen(port,() => { 
-    console.log(`http://localhost:${port}`)
- })
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
